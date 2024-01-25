788686e452c5e79dca7c56a82450d6f6eef8a491
Hon Loon <tenghonloon@hotmail.com>
2024-01-25 20:36:34 +0800

    RabbitMq C# Producer Python Consumer

---
+++ TestRabbitMq/Consumer/testRabbitMq/main.py
@@ -0,0 +1,21 @@
+import pika
+
+
+def on_request(ch, method, props, body):
+    content = body.decode()
+    print(f"Received {content}")
+    response = f"Processed {content}"
+    ch.basic_publish(exchange='',
+                     routing_key=props.reply_to,
+                     properties=pika.BasicProperties(correlation_id=props.correlation_id),
+                     body=str(response))
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+    # ch.close()
+
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+channel.basic_consume(queue='request_queue', on_message_callback=on_request, auto_ack=False)
+
+print("Awaiting RPC requests")
+channel.start_consuming()
---
+++ TestRabbitMq/TestRabbitMq/Program.cs
@@ -0,0 +1,74 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace TestRabbitMq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var rabbitMQClient = new RabbitMQClient();
+
+            for(var i = 0; i < 100000; i++)
+            {
+                string message = $"Hello World! {i}";
+                rabbitMQClient.SendRequest(message);
+            }
+
+            Console.WriteLine("Press [enter] to exit.");
+            Console.ReadLine();
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+
+    public class RabbitMQClient
+    {
+        private readonly IConnection connection;
+        private readonly IModel channel;
+        private readonly string requestQueueName;
+        private readonly string replyQueueName;
+        private readonly EventingBasicConsumer consumer;
+        private readonly string correlationId;
+
+        public RabbitMQClient()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            connection = factory.CreateConnection();
+            channel = connection.CreateModel();
+            requestQueueName = "request_queue";
+            channel.QueueDeclare(queue: requestQueueName, durable: false, exclusive: false, autoDelete: true, arguments: null);
+
+            replyQueueName = channel.QueueDeclare().QueueName;
+            consumer = new EventingBasicConsumer(channel);
+            correlationId = Guid.NewGuid().ToString();
+
+            consumer.Received += (model, ea) =>
+            {
+                if (ea.BasicProperties.CorrelationId == correlationId)
+                {
+                    var response = Encoding.UTF8.GetString(ea.Body.ToArray());
+                    Console.WriteLine("Response: " + response);
+
+                    //channel.Close();
+                    //connection.Close();
+                }
+            };
+
+            channel.BasicConsume(consumer: consumer, queue: replyQueueName, autoAck: true);
+        }
+
+        public void SendRequest(string message)
+        {
+            var props = channel.CreateBasicProperties();
+            props.CorrelationId = correlationId;
+            props.ReplyTo = replyQueueName;
+
+            var messageBytes = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish(exchange: "", routingKey: requestQueueName, basicProperties: props, body: messageBytes);
+            Console.WriteLine("Sent: " + message);
+        }
+    }
+}
